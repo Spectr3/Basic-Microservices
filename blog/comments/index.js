36913fbb1fbc5ef3bc015bf1ba23856be4a1463b
Martyn Price <martyncprice@gmail.com>
2021-01-02 16:48:33 +0000

    Update comments and add cors

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes} = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -11,7 +14,8 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
+    console.log('Comment sent');
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
     
@@ -20,7 +24,23 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({ id: commentId, content });
     commentsByPostId[req.params.id] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
+    console.log('Event Sent');
+
     res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+    console.log('Recieved event', req.body.type);
+
+    res.send({});
 });
 
 app.listen(4001, () => {
