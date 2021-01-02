6652c41cbeed7dc52c4b308dcfa2a429cd7f52d5
Martyn Price <martyncprice@gmail.com>
2021-01-02 16:48:48 +0000

    Update posts and add cors

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
@@ -12,7 +15,8 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
+    console.log('Post sent');
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
@@ -20,7 +24,21 @@ app.post('/posts', (req, res) => {
         id, title
     };
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    });
+    console.log('Sent event');
+
     res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+    console.log('Recieved event', req.body);
+
+    res.send({});
 });
 
 app.listen(4000, () => {
